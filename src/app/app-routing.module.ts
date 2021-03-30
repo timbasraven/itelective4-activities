5a8e9590eaaa98cfc5042327850a6d468d41aaf5
unknown <201810624@gordoncollege.edu.ph>
2021-03-30 22:59:17 +0800

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'loginscreen',
+    loadChildren: () => import('./loginscreen/loginscreen.module').then( m => m.LoginscreenPageModule)
+  },
 ];
 
 @NgModule({
